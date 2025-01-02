f234ead7bae54e5492e406f95fe28185e2029b70
Elluminati212 <vpatel@elluminatiinc.com>
2025-01-02 10:29:05 +0530

    main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 from src.routers import user_controller
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.trustedhost import TrustedHostMiddleware
-
+from pydantic_settings import BaseSettings
 
 
 app = FastAPI()
@@ -23,6 +23,25 @@ app.add_middleware(
 )
 
 app.include_router(user_controller.router) 
+
+def get_mongo_client():
+
+    client = BaseSettings.db_url
+    return client
+
+@app.route('/', methods=['GET'])
+def get_prediction():
+    try:
+        # Connect to MongoDB
+        client = get_mongo_client()
+        db = client['WriteDB'] # Replace with your database name
+        collection = db['prediction'] # Replace with your collection name
+
+        # Fetch all predictions from the database
+        predictions = list(collection.find({}, {"_id": 0, "latitude": 1, "longitude": 1, "cluster": 1, "trip_type": 1, "booking_type": 1, "ds": 1, "price_min": 1, "price_max": 1})) # Exclude MongoDB _id field
+        print(db.prediction.find())
+    except Exception as e:
+        return {"error": str(e)}
 
 if __name__ == "__main__":
     import uvicorn
