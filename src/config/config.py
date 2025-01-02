63e8abfff1d6a31861b23281f3687b81d70ab94c
Elluminati212 <vpatel@elluminatiinc.com>
2025-01-02 10:29:58 +0530

    config.py

--- src/config/config.py
+++ src/config/config.py
@@ -1,4 +1,4 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     db_url: str = "mongodb+srv://vpatel179:LqpC2zz4rO7pmtkR@cluster0.ejksq.mongodb.net/"
